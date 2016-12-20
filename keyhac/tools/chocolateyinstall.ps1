1ef4698b0e2707cc40b18883b3e225cc3e37a2db
geo_trader <geometrytrader@gmail.com>
2016-12-20 22:33:14 +0900

    add install scripts for keyhac

---
+++ keyhac/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'keyhac'
+  url            = 'http://crftwr.github.io/keyhac/download/keyhac_176.zip'
+  checksum = '40E4108DAABDC5EFF2165DAE62C11CB9FB5DCCCA2D27067DDCE10B1820D06B35'
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
