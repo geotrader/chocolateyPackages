a72c5e06bad035afb65e11eb5231b8e10150ced2
geo_trader <geometrytrader@gmail.com>
2016-12-20 22:31:12 +0900

    add install scripts for CraftFiler

---
+++ cfiler/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'cfiler'
+  url            = 'http://crftwr.github.io/cfiler/download/cfiler_245.zip'
+  checksum = 'C05D967CEAE1777608625AF269E737F9C76A1EF2B7AF31B509D6BAA017746981'
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
