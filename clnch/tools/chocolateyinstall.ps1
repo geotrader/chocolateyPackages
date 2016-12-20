ace5889f464f16eeef8ef2c35467bb3d1f7cc8a9
geo_trader <geometrytrader@gmail.com>
2016-12-20 22:31:45 +0900

    add install scripts for CraftLaunch

---
+++ clnch/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'clnch'
+  url            = 'http://crftwr.github.io/clnch/download/clnch_333.zip'
+  checksum = 'B8F2A1AFBFF428FDAC8728A0F5BD7400ED4C0D063B905A1AD35322141C9D1261'
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
