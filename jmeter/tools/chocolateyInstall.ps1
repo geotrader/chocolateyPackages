d0614b3c863d1067cdd435f93f8ed0916f328007
geoTrader <keita.kidani@jrits.ricoh.co.jp>
2017-01-11 09:37:17 +0900

    Jmeterのインストールパッケージを追加
    - インストールはできたが、chocolatey/bin以下へのexeの配置方法が不明

---
+++ jmeter/tools/chocolateyInstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'jmeter'
+  url            = 'http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-3.1.zip'
+  checksum = '71DDFDFC324D8877C4DA0DFBE9A860C5C10F5D233F4D84B2055252C0CDF76A6E'
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
