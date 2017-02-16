8b9af3a50e782b52d0ead0f5c8336d434586b96e
geoTrader <keita.kidani@jrits.ricoh.co.jp>
2017-02-16 10:42:54 +0900

    add 64bit case.(its same value)

--- jmeter/tools/chocolateyInstall.ps1
+++ jmeter/tools/chocolateyInstall.ps1
@@ -5,8 +5,11 @@ $toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'jmeter'
   url            = 'http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-3.1.zip'
-  checksum = '71DDFDFC324D8877C4DA0DFBE9A860C5C10F5D233F4D84B2055252C0CDF76A6E'
+  url64bit       = 'http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-3.1.zip'
+  checksum       = '71DDFDFC324D8877C4DA0DFBE9A860C5C10F5D233F4D84B2055252C0CDF76A6E'
   checksumType   = 'sha256'
+  checksum64     = '71DDFDFC324D8877C4DA0DFBE9A860C5C10F5D233F4D84B2055252C0CDF76A6E'
+  checksumType64 = 'sha256'
   unzipLocation  = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
@@ -16,5 +19,5 @@ try {
   cp $env:ChocolateyInstall\lib\$packageName\tools\jmeter.bat $env:ChocolateyInstall\bin
 } catch {
   Write-ChocolateyFailure $packageName $($_.Exception.Message)
-  throw 
+  throw
 }
