1cc43e73ef25631b9584efffca0452e024191531
geoTrader <keita.kidani@jrits.ricoh.co.jp>
2017-04-21 13:47:17 +0900

    add zadig package

---
+++ zadig/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'zadig'
+  fileFullPath  = Join-Path $toolsPath 'zadig.exe'  
+  url            = 'http://zadig.akeo.ie/downloads/zadig-2.3.exe'
+  url64bit       = 'http://zadig.akeo.ie/downloads/zadig-2.3.exe'
+  checksum       = 'E2695E5732A8B512DB00B5B7D5DB4854195E82D343633C245E50F26363CAFB2E'
+  checksumType   = 'sha256'
+  checksum64     = 'E2695E5732A8B512DB00B5B7D5DB4854195E82D343633C245E50F26363CAFB2E'
+  checksumType64 = 'sha256'
+}
+Get-ChocolateyWebFile @packageArgs
