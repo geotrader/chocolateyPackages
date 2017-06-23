26e61d805ae11b56fb0eef1e752081e45354a6a0
geoTrader <keita.kidani@jrits.ricoh.co.jp>
2017-06-23 13:13:04 +0900

    add teraterm.portable package

---
+++ teraterm.portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = 'teraterm.portable'
+  url            = 'https://osdn.net/projects/ttssh2/downloads/67769/teraterm-4.95.zip'
+  checksum       = '2f06011eb3e7faa8f4f004b9fb5262b57751ec02c762cb92b4bd8a615f44664c'
+  checksumType   = 'sha256'
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
