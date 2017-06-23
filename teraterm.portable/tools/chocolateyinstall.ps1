d99567691619b9dbeb77551140f22dafb05d1612
geoTrader <keita.kidani@jrits.ricoh.co.jp>
2017-06-23 13:41:14 +0900

    add exclude shims

--- teraterm.portable/tools/chocolateyinstall.ps1
+++ teraterm.portable/tools/chocolateyinstall.ps1
@@ -10,3 +10,24 @@ $packageArgs = @{
   unzipLocation  = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
+
+# exclude generate shim(s)
+$ignoreFiles = @(
+  'Collector.exe.ignore',
+  'Collector/Collector.exe.ignore',
+  'Collector_org.exe.ignore',
+  'Collector/Collector_org.exe.ignore',
+  'cygterm.exe.ignore',
+  'cygterm+-i686/cygterm.exe.ignore',
+  'cygterm+-x86_64/cygterm.exe.ignore',
+  'keycode.exe.ignore',
+  'ttpmenu.exe.ignore',
+  'cyglaunch.exe.ignore'
+)
+
+
+$zipName = [System.IO.Path]::GetFileNameWithoutExtension($packageArgs.url)
+
+foreach($ignoreFile in $ignoreFiles) {
+  New-Item "${toolsPath}\${zipName}\${ignoreFile}" -type file -force | Out-Null
+}
