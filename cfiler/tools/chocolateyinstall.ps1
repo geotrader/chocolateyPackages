6805274e5b8af37919745d39303becd3bfbd4f28
K.K <k-kidani@sky-co.jp>
2017-10-30 17:02:40 +0900

    update cfiler and keyhac
    switch sha256 to sha512

--- cfiler/tools/chocolateyinstall.ps1
+++ cfiler/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'cfiler'
   url            = 'http://crftwr.github.io/cfiler/download/cfiler_245.zip'
-  checksum = 'C05D967CEAE1777608625AF269E737F9C76A1EF2B7AF31B509D6BAA017746981'
-  checksumType   = 'sha256'
+  checksum = 'B9F256C143C6CA3A4DC4C9BB2BAE17A86A5070971E71F956C8D4E7B45992E5A2549CF14740365CEEE6013EE9325C4235E014E90F8095F0BF656EA17D589276FE'
+  checksumType   = 'sha512'
   unzipLocation  = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
--- keyhac/tools/chocolateyinstall.ps1
+++ keyhac/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   packageName    = 'keyhac'
   url            = 'http://crftwr.github.io/keyhac/download/keyhac_176.zip'
-  checksum = '40E4108DAABDC5EFF2165DAE62C11CB9FB5DCCCA2D27067DDCE10B1820D06B35'
-  checksumType   = 'sha256'
+  checksum = '56F8AB2AC758C68F2B3C5E635F88F2E1E38B048D5E9A5B1CA5C5E1BAB7A8C7A9537EEFDB93FC4E3F79FBFC168E030AF4AFBCE07213654EF6E06A0E02C4533EA3'
+  checksumType   = 'sha512'
   unzipLocation  = $toolsPath
 }
 Install-ChocolateyZipPackage @packageArgs
