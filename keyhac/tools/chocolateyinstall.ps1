f1f7860aaaf403a927222df0133f5668e6b9356f
geo_trader <geometrytrader@gmail.com>
2017-10-31 13:16:58 +0900

    fix download url and switch http to https

--- keyhac/tools/chocolateyinstall.ps1
+++ keyhac/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'keyhac'
-  url            = 'http://crftwr.github.io/keyhac/download/keyhac_176.zip'
+  url            = 'https://crftwr.github.io/keyhac/download/keyhac_177.zip'
   checksum = '56F8AB2AC758C68F2B3C5E635F88F2E1E38B048D5E9A5B1CA5C5E1BAB7A8C7A9537EEFDB93FC4E3F79FBFC168E030AF4AFBCE07213654EF6E06A0E02C4533EA3'
   checksumType   = 'sha512'
   unzipLocation  = $toolsPath
