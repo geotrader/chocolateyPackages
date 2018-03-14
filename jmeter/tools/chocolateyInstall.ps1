1345d61624225ea444d91e76e255d10e74e37f53
geo_trader <geometrytrader@gmail.com>
2018-03-14 13:03:29 +0900

    fix dependencies javaruntime 9 to 8
    some link update http to https

--- jmeter/tools/chocolateyInstall.ps1
+++ jmeter/tools/chocolateyInstall.ps1
@@ -4,7 +4,7 @@ $toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'jmeter'
-  url            = 'http://archive.apache.org/dist/jmeter/binaries/apache-jmeter-4.0.zip'
+  url            = 'https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-4.0.zip'
   checksum       = 'acb4489bc875513ccc467782b3872e4adc048cd0fc8f375337dca8da82d48a83374ebfa749a8d3ae65163e32b5d11dfa44e557ca9b103b43d365dc6a6fc0ff56'
   checksumType   = 'sha512'
   unzipLocation  = $toolsPath
