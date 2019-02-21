f6065d235f4fe299adc00226a0f9d55301d52020
K.K <geometrytrader@gmail.com>
2019-02-21 15:06:45 +0900

    update to 2.49

--- cfiler/tools/chocolateyinstall.ps1
+++ cfiler/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'cfiler'
-  url            = 'https://crftwr.github.io/cfiler/download/cfiler_247.zip'
-  checksum = '8873F26FB9988B1340DCBC4D34FBBEA7FF19A8264C9FAE2AB2F684A1D49F8D1C0D24A1EEE95305FB20171196C92F8B8EF5AA432DA6F3427E61BF32220D804A7B'
+  url            = 'https://crftwr.github.io/cfiler/download/cfiler_249.zip'
+  checksum	 = '934558C5313990620CD23FEE25C668BF1FAC1D5663EC41E45B2E93BD4F5E832A055A5FEAAB93928A0C87582BF218024CF53728047D7FFB81FB1ED23618DFAE61'
   checksumType   = 'sha512'
   unzipLocation  = $toolsPath
 }
