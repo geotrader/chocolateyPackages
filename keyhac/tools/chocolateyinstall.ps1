0ed3ebe051abdab96f8834932c75be3d3debb162
geotrader <geometrytrader@gmail.com>
2021-03-10 07:25:09 +0900

    upgrade keyhac to 1.82

--- keyhac/tools/chocolateyinstall.ps1
+++ keyhac/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsPath   = Split-Path -parent $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
   packageName    = 'keyhac'
-  url            = 'https://crftwr.github.io/keyhac/download/keyhac_177.zip'
-  checksum = '56F8AB2AC758C68F2B3C5E635F88F2E1E38B048D5E9A5B1CA5C5E1BAB7A8C7A9537EEFDB93FC4E3F79FBFC168E030AF4AFBCE07213654EF6E06A0E02C4533EA3'
+  url            = 'https://crftwr.github.io/keyhac/download/keyhac_182.zip'
+  checksum       = 'A5BC51069E0358B551227AF010256CCBD2043F46663B4EEB59CDC55A67186099DB27BB838E0E75392A70B4C9DEEC7D915D34ECE266CD93773A6E9CD919EF32DF'
   checksumType   = 'sha512'
   unzipLocation  = $toolsPath
 }
